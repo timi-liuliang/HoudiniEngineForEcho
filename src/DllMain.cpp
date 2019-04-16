bb4ce44b757597dde7a9890f4cd654d7a575281f
Liang Liu <qq79402005@gmail.com>
2019-04-16 16:35:58 +0800

    Support HoudiniDigitalAsset Node #5

--- src/DllMain.cpp
+++ src/DllMain.cpp
@@ -1,8 +1,12 @@
 #include "DllMain.h"
 #include <engine/core/main/module.h>
 
-bool loadHoudiniEnginePlugin()
+bool loadHoudiniEnginePlugin(Echo::Engine* engine)
 {
+	// replace instances
+	engine->initializeDll();
+
+	// register modules
     Echo::registerModules();
 
 	return true;
--- src/DllMain.h
+++ src/DllMain.h
@@ -6,10 +6,12 @@
     #define ECHO_PLUGIN_EXPORT __attribute__((visibility("default")))
 #endif
 
+#include <engine/core/main/Engine.h>
+
 extern "C"
 {
 	// load houdini plugin
-	bool ECHO_PLUGIN_EXPORT loadHoudiniEnginePlugin();						
+	bool ECHO_PLUGIN_EXPORT loadHoudiniEnginePlugin(Echo::Engine* engine);
 
 	// unload houdini plugin
 	bool ECHO_PLUGIN_EXPORT unloadHoudiniEnginePlugin();
